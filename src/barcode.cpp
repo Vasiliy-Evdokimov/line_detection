ee14847ffb72fda5f92d07e12e8fdd0ab0af4f9a
Vasiliy Evdokimov <vevdokimov@semargl.pro>
2023-09-26 16:44:16 +0300

    Linear barcodes detection; distance to datamatrix

--- src/barcode.cpp
+++ src/barcode.cpp
@@ -6,14 +6,23 @@
  */
 
 #include <iostream>
+#include <string>
+#include <cmath>
 
 #include "opencv2/opencv.hpp"
 
+#include <zbar.h>	//	порядок важен - сначала подключаем zbar, потом zxing, иначе конфликт
+
 #include "ZXing/ReadBarcode.h"
+#include "ZXing/BarcodeFormat.h"
 
 #include "defines.hpp"
 
-using namespace std;
+const double FOCAL_DIST = 2.8;
+const double MATRIX_W = 3.7;
+const double MATRIX_H = 2.7;
+const double BARCODE_W = 45;
+const double DATAMATRIX_W = 26;	//	31
 
 inline ZXing::ImageView ImageViewFromMat(const cv::Mat& image)
 {
@@ -34,11 +43,22 @@ inline ZXing::ImageView ImageViewFromMat(const cv::Mat& image)
 
 inline ZXing::Results ReadBarcodes(const cv::Mat& image, const ZXing::DecodeHints& hints = {})
 {
-	return ZXing::ReadBarcodes(ImageViewFromMat(image), hints);
+	ZXing::DecodeHints hints2;
+	hints2.setTryHarder(true);
+	hints2.setFormats(ZXing::BarcodeFormat::Any);
+	//
+	return ZXing::ReadBarcodes(ImageViewFromMat(image), hints2);
+}
+
+double GetPointDist(cv::Point pt1, cv::Point pt2) {
+	return sqrt(std::pow(pt2.x - pt1.x, 2) + std::pow(pt2.y - pt1.y, 2));
 }
 
 inline void DrawResult(cv::Mat& img, ZXing::Result res)
 {
+//	if (res.format() != ZXing::BarcodeFormat::DataMatrix)
+//		return;
+
 	auto pos = res.position();
 	auto zx2cv = [](ZXing::PointI p) { return cv::Point(p.x, p.y); };
 	auto contour = std::vector<cv::Point>{zx2cv(pos[0]), zx2cv(pos[1]), zx2cv(pos[2]), zx2cv(pos[3])};
@@ -46,31 +66,67 @@ inline void DrawResult(cv::Mat& img, ZXing::Result res)
 	int npts = contour.size();
 
 #ifndef NO_GUI
+
 	cv::Moments M = cv::moments(contour);
 	cv::Point center(M.m10 / M.m00, M.m01 / M.m00);
 	cv::circle(img, center, 3, CLR_YELLOW, -1, cv::LINE_AA);
+	//
+	cv::polylines(img, &pts, &npts, 1, true, CLR_GREEN);
+	cv::putText(img, res.text(), zx2cv(pos[3]) + cv::Point(0, 20),
+		cv::FONT_HERSHEY_DUPLEX, 0.5, CLR_GREEN);
 
-	cv::polylines(img, &pts, &npts, 1, true, CV_RGB(0, 255, 0));
-	cv::putText(img, res.text(), zx2cv(pos[3]) + cv::Point(0, 20), cv::FONT_HERSHEY_DUPLEX, 0.5, CV_RGB(0, 255, 0));
+	if (res.format() == ZXing::BarcodeFormat::DataMatrix) {
 
-//	cv::line(img, zx2cv(pos[0]), zx2cv(pos[1]),	CLR_RED, 2, cv::LINE_AA, 0);
-//	cv::line(img, zx2cv(pos[1]), zx2cv(pos[2]),	CLR_CYAN, 2, cv::LINE_AA, 0);
+		cv::Point p1(center.x, img.rows / 2);	//	img.cols / 2
+		cv::line(img, p1, center, CLR_YELLOW, 1, cv::LINE_AA, 0);
+
+		double dist1 = GetPointDist(zx2cv(pos[0]), zx2cv(pos[1]));
+		double dist2 = GetPointDist(center, p1) * (DATAMATRIX_W / dist1);
+
+//		cv::putText(img, std::to_string(dist1), zx2cv(pos[3]) + cv::Point(0, 40),
+//			cv::FONT_HERSHEY_DUPLEX, 0.5, CLR_GREEN);
+		//
+		//	Object height on sensor (mm) = (Sensor height (mm) * Object height (pixels)) / Sensor height (pixels)
+//		double ohs = (MATRIX_H * dist1) / img.rows;
+		//	Distance to Object =  (Real Object height * Focal Length (mm)) / Object height on sensor (mm)
+//		double dto = (DATAMATRIX_W * FOCAL_DIST) / ohs;
+		//	Real Object height = ((Distance to Object * Object height on sensor (mm)) / Focal Length (mm)
+//		double roh = (dto * ohs) / FOCAL_DIST;
+		//
+//		cv::putText(img, std::to_string(dto), zx2cv(pos[3]) + cv::Point(0, 60),
+//				cv::FONT_HERSHEY_DUPLEX, 0.5, CLR_GREEN);
+		cv::putText(img, std::to_string(dist2), zx2cv(pos[3]) + cv::Point(0, 40),
+						cv::FONT_HERSHEY_DUPLEX, 0.5, CLR_GREEN);
+		//
+//		cv::line(img, zx2cv(pos[0]), zx2cv(pos[1]), CLR_MAGENTA, 1, cv::LINE_AA, 0);
+//		cv::line(img, zx2cv(pos[1]), zx2cv(pos[2]), CLR_CYAN, 1, cv::LINE_AA, 0);
+
+	} else
+	//
+	if (res.format() == ZXing::BarcodeFormat::Codabar) {
+
+		//
+
+	}
+
 #endif
 
 }
 
-string last_bc;
+std::string last_bc;
 int cnt = 0;
 
 void find_barcodes(cv::Mat& imgColor)
 {
+
 	auto results = ReadBarcodes(imgColor);
 	for (auto& r : results) {
 		DrawResult(imgColor, r);
 		if (last_bc != r.text()) {
 			last_bc = r.text();
-			cout << cnt << " Barcode detected = " << last_bc << endl;
+			std::cout << cnt << " Barcode detected = " << last_bc << std::endl;
 			cnt++;
 		}
 	}
+
 }
